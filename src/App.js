7eedc79dfe70a5af03ed76729f244f561fdd172a
Porte1997 <shubhamporte0@gmail.com>
2023-07-05 09:26:57 +0530

    My First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppContext } from "./Context/contextApi";
+
+import Feed from './Components/Feed';
+import Header from './Components/Header';
+import SearchResult from './Components/SearchResult';
+import VideoDetails from './Components/VideoDetails';
+
+const App = () => {
+  return (
+    <AppContext>
+        <BrowserRouter>
+          <div className="flex flex-col h-full">
+            <Header />
+            <Routes>
+              <Route path="/" exact element={<Feed />} />
+              <Route path="/searchResult/:searchQuery" element={<SearchResult />} />
+              <Route path="/video/:id" element={<VideoDetails />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AppContext>
+  )
+}
+
+export default App; 
